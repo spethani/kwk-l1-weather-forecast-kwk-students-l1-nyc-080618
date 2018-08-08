b1cb6b1aa5c5a029c930791bc7bf4c991c08b730
spethani <sarahpethani@gmail.com>
2018-08-08 18:04:32 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -31,7 +31,7 @@ end
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 def weather_report(weather)
-  puts "The weather in #{weather[:city]} is #{weather[:description]}.The temperature is #{weather[:temperature_farenheit]} degrees Fahrenheit."
+  puts "The weather in #{weather[:city]} is #{weather[:description]}. The temperature is #{weather[:temperature_farenheit]} degrees Fahrenheit."
 end
 
 #4. Write a method that takes in the weather hash as an argument. 
